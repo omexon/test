873e8c82220aacaeeea85927a10a23fb8ddca8b9
CoRex <dev@corex.dk>
2019-09-26 07:16:50 +0200

    One more test

--- src/Installer.php
+++ src/Installer.php
@@ -9,7 +9,17 @@ class Installer
         print(__CLASS__ . '::' . __FUNCTION__ . "\n");
     }
 
+    public static function postPackageUpdate()
+    {
+        print(__CLASS__ . '::' . __FUNCTION__ . "\n");
+    }
+
     public static function postAutoloadDump()
+    {
+        print(__CLASS__ . '::' . __FUNCTION__ . "\n");
+    }
+
+    public static function postUpdate()
     {
         print(__CLASS__ . '::' . __FUNCTION__ . "\n");
     }
